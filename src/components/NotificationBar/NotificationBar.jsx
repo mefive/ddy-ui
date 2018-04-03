a9416ea7af57c64cb589f499642d7595dc1fd8d5
liucong211463 <liucong211463@sogou-inc.com>
2018-04-03 18:48:55 +0800

    fix: NotificationBar

--- src/components/NotificationBar/NotificationBar.jsx
+++ src/components/NotificationBar/NotificationBar.jsx
@@ -1,66 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 
 import './style/index.scss';
 import Animate from '../Animate/Animate';
+import Notification from './Notification';
 
 const propTypes = {
-  wait: PropTypes.number,
-  onRemove: PropTypes.func,
   dataSource: PropTypes.arrayOf(PropTypes.shape({})),
-  className: PropTypes.string,
-  type: PropTypes.string,
-  message: PropTypes.string,
 };
 
 const defaultProps = {
-  wait: 3000,
-  onRemove: () => {},
   dataSource: [],
-  className: null,
-  type: Notification.type.SUCC,
-  message: null,
 };
 
-export class Notification extends React.PureComponent {
-  componentDidMount() {
-    this.waitTimer = setTimeout(
-      () => this.props.onRemove(),
-      this.props.wait,
-    );
-  }
-
-  componentWillUnmount() {
-    if (this.timer) {
-      clearTimeout(this.timer);
-      this.timer = null;
-    }
-  }
-
-  render() {
-    return (
-      <div
-        className={classNames(
-          'notification',
-          { 'alert-success': this.props.type === Notification.type.SUCC },
-          { 'alert-error': this.props.type === Notification.type.ERROR },
-          this.props.className,
-        )}
-      >
-        {this.props.message}
-      </div>
-    );
-  }
-}
-
-Notification.propTypes = propTypes;
-Notification.defaultProps = defaultProps;
-
-Notification.type = {
-  ERROR: 'error',
-  SUCC: 'succ',
-};
 
 const NotificationBar = props => (
   <div className="notification-bar">
@@ -82,7 +34,6 @@ const NotificationBar = props => (
 
 NotificationBar.propTypes = propTypes;
 NotificationBar.defaultProps = defaultProps;
-
 NotificationBar.Notification = Notification;
 
 export default NotificationBar;
