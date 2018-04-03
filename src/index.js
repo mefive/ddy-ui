022e5f6a984f1ba18e27b7e64d33e20fd9415bfd
liucong211463 <liucong211463@sogou-inc.com>
2018-04-03 17:02:24 +0800

    fix;

--- src/index.js
+++ src/index.js
@@ -1,37 +1,77 @@
-export Alert from './components/Alert';
-export Animate from './components/Animate';
-export Calendar from './components/Calendar';
-export Chart from './components/Chart';
-export Clickable from './components/Clickable';
-export CodeEditor from './components/CodeEditor';
-export Confirm from './components/Confirm';
-export DatePicker from './components/DatePicker';
-export DateRangePicker from './components/DateRangePicker';
-export DateRangeSelect from './components/DateRangeSelect';
-export DoubleBounce from './components/DoubleBounce';
-export Draggable from './components/Draggable';
-export FileUploader from './components/FileUploader';
-export FlexWrapper from './components/FlexWrapper';
-export Form from './components/Form';
-export FullScreenModal from './components/FullScreenModal';
-export ImagesUploader from './components/ImagesUploader';
-export Input from './components/Input';
-export Loading from './components/Loading';
-export Modal from './components/Modal';
-export NotificationBar from './components/NotificationBar';
-export NumberFlip from './components/NumberFlip';
-export OdometerNumber from './components/OdometerNumber';
-export Pagination from './components/Pagination';
-export Popover from './components/Popover';
-export Portal from './components/Portal';
-export RadioGroup from './components/RadioGroup';
-export Scrollable from './components/Scrollable';
-export Search from './components/Search';
-export Select from './components/Select';
-export Table from './components/Table';
-export Tabs from './components/Tabs';
-export TextArea from './components/TextArea';
-export ToggleButton from './components/ToggleButton';
-export Tooltip from './components/Tooltip';
-export Tree from './components/Tree';
-export Trigger from './components/Trigger';
+import Alert from './components/Alert';
+import Animate from './components/Animate';
+import Calendar from './components/Calendar';
+import Chart from './components/Chart';
+import Clickable from './components/Clickable';
+import CodeEditor from './components/CodeEditor';
+import Confirm from './components/Confirm';
+import DatePicker from './components/DatePicker';
+import DateRangePicker from './components/DateRangePicker';
+import DateRangeSelect from './components/DateRangeSelect';
+import DoubleBounce from './components/DoubleBounce';
+import Draggable from './components/Draggable';
+import FileUploader from './components/FileUploader';
+import FlexWrapper from './components/FlexWrapper';
+import Form from './components/Form';
+import FullScreenModal from './components/FullScreenModal';
+import ImagesUploader from './components/ImagesUploader';
+import Input from './components/Input';
+import Loading from './components/Loading';
+import Modal from './components/Modal';
+import NotificationBar from './components/NotificationBar';
+import NumberFlip from './components/NumberFlip';
+import OdometerNumber from './components/OdometerNumber';
+import Pagination from './components/Pagination';
+import Popover from './components/Popover';
+import Portal from './components/Portal';
+import RadioGroup from './components/RadioGroup';
+import Scrollable from './components/Scrollable';
+import Search from './components/Search';
+import Select from './components/Select';
+import Table from './components/Table';
+import Tabs from './components/Tabs';
+import TextArea from './components/TextArea';
+import ToggleButton from './components/ToggleButton';
+import Tooltip from './components/Tooltip';
+import Tree from './components/Tree';
+import Trigger from './components/Trigger';
+
+export {
+  Alert,
+  Animate,
+  Calendar,
+  Chart,
+  Clickable,
+  CodeEditor,
+  Confirm,
+  DatePicker,
+  DateRangePicker,
+  DateRangeSelect,
+  DoubleBounce,
+  Draggable,
+  FileUploader,
+  FlexWrapper,
+  Form,
+  FullScreenModal,
+  ImagesUploader,
+  Input,
+  Loading,
+  Modal,
+  NotificationBar,
+  NumberFlip,
+  OdometerNumber,
+  Pagination,
+  Popover,
+  Portal,
+  RadioGroup,
+  Scrollable,
+  Search,
+  Select,
+  Table,
+  Tabs,
+  TextArea,
+  ToggleButton,
+  Tooltip,
+  Tree,
+  Trigger,
+};
--- src/script/export.js
+++ src/script/export.js
@@ -11,13 +11,15 @@ fs.readdir(path.join(__dirname, '../components'), (err, files) => {
     .filter(f => ['m', 'style'].indexOf(f) === -1)
     .map(f => f.replace(/\.jsx/, ''));
 
-  const codes = components.map(c => `export ${c} from './components/${c}';`);
+  const codes = components.map(c => `import ${c} from './components/${c}';`);
+
+  codes.push(`\nexport {\n${components.map(c => `  ${c},`).join('\n')}\n};\n`);
 
   console.log(codes);
 
-  fs.writeFile(path.resolve(__dirname, '../index.js'), codes.join('\n'), (err) => {
-    if (err) {
-      console.log('write err', err);
+  fs.writeFile(path.resolve(__dirname, '../index.js'), codes.join('\n'), (errWrite) => {
+    if (errWrite) {
+      console.log('write err', errWrite);
       return;
     }
 
