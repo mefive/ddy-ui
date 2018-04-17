68522c10260ed842023e781d9d983f765f9125a0
liucong211463 <liucong211463@sogou-inc.com>
2018-04-17 15:00:21 +0800

    fix: Portal using React 16 createPortal

--- src/Portal.jsx
+++ src/Portal.jsx
@@ -1,12 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-
-/* eslint-disable */
-import {
- unstable_renderSubtreeIntoContainer,
- unmountComponentAtNode,
-} from 'react-dom';
-/* eslint-enable */
 
 import classNames from 'classnames';
 
@@ -24,8 +18,10 @@ const defaultProps = {
   className: null,
 };
 
-class Portal extends Component {
-  componentDidMount() {
+class Portal extends React.PureComponent {
+  constructor(props) {
+    super(props);
+
     const { getContainer } = this.props;
 
     if (getContainer) {
@@ -33,47 +29,25 @@ class Portal extends Component {
     } else {
       this.container = document.createElement('div');
       document.body.appendChild(this.container);
-      this.renderInner();
     }
+  }
 
+  componentDidMount() {
     this.props.onContainerChange(this.container.parentNode);
-
-    if (this.props.children != null) {
-      this.renderInner();
-    }
-  }
-
-  componentDidUpdate() {
-    this.renderInner();
-  }
-
-  componentWillUnmount() {
-    const { container } = this;
-    unmountComponentAtNode(container);
-    if (container.parentNode != null) {
-      container.parentNode.removeChild(container);
-    }
-    this.container = null;
-  }
-
-  renderInner() {
-    unstable_renderSubtreeIntoContainer(
-      this,
-      React.cloneElement(
-        this.props.children,
-        {
-          className: classNames(
-            this.props.children.props ? this.props.children.props.className : null,
-            this.props.className,
-          ),
-        },
-      ),
-      this.container,
-    );
   }
 
   render() {
-    return null;
+    const children = React.cloneElement(
+      this.props.children,
+      {
+        className: classNames(
+          this.props.children.props ? this.props.children.props.className : null,
+          this.props.className,
+        ),
+      },
+    );
+
+    return ReactDOM.createPortal(children, this.container);
   }
 }
 
