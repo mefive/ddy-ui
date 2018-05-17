8fcbccbe2b317216971cab848d74166db641f48d
liucong211463 <liucong211463@sogou-inc.com>
2018-05-17 19:12:25 +0800

    fix: eslint

--- src/Switch/Switch.jsx
+++ src/Switch/Switch.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import addClass from 'dom-helpers/class/addClass';
 
 import Clickable from '../Clickable';
 
