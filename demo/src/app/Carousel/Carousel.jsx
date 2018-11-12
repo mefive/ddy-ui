f6c22a0d67bb7be6dc55b70bc9e4e2b37d142430
mefive <mark.liu.cong@gmail.com>
2018-11-12 19:41:36 +0800

    feat: Carousel2

--- demo/src/app/Carousel/Carousel.jsx
+++ demo/src/app/Carousel/Carousel.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import pic0 from './images/0.png';
 import pic1 from './images/1.png';
 import pic2 from './images/2.png';
 import pic3 from './images/3.png';
@@ -12,26 +11,25 @@ import pic8 from './images/8.png';
 import pic9 from './images/9.png';
 import ShowcaseContainer from '../ShowcaseContainer/ShowcaseContainer';
 import Carousel from '../../../../src/Carousel2';
-
-const dataSource = [pic0, pic1, pic2];
+import Image from '../../../../src/Image';
 
 class CarouselShowcase extends React.PureComponent {
+  state = {
+    dataSource: [pic1, pic2, pic3, pic4, pic5, pic6, pic6, pic7, pic8, pic9],
+  };
+
   render() {
     return (
       <ShowcaseContainer title="Carousel">
         <div
-          style={{ width: 500, height: 500, margin: '0 auto' }}
+          style={{ width: 400, height: 400, margin: '0 auto' }}
           className="border"
         >
           <Carousel
-            // dataSource={Array(2).fill(0).map((n, index) => ({
-            //   render: () => (
-            //     <div data-key={index} style={{ lineHeight: '500px', textAlign: 'center', fontSize: '40px' }}>
-            //       {index}
-            //     </div>
-            //   ),
-            // }))}
-            dataSource={dataSource}
+            dataSource={this.state.dataSource}
+            renderItem={url => (
+              <Image src={url} mode={Image.MODE_OUTER_CUT} width="100%" height="100%" />
+            )}
             hasPageTurner
           />
         </div>
--- src/Carousel2/Carousel.jsx
+++ src/Carousel2/Carousel.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import Image from '../Image';
+import Clickable from '../Clickable';
 
 import './style.scss';
 
@@ -10,25 +12,31 @@ class Carousel extends React.PureComponent {
       PropTypes.string,
       PropTypes.shape({}),
     ])).isRequired,
-
+    renderItem: PropTypes.func,
     // interval: PropTypes.number,
-    // hasPageTurner: PropTypes.bool,
+    enableLoop: PropTypes.bool,
+    hasPageTurner: PropTypes.bool,
+    cursorIndex: PropTypes.number,
   };
 
   static defaultProps = {
+    renderItem: null,
     // interval: null,
-    // hasPageTurner: false,
+    enableLoop: false,
+    hasPageTurner: false,
+    cursorIndex: 0,
   };
 
   constructor(props) {
     super(props);
 
     this.container = React.createRef();
-  }
 
-  state = {
-    width: 0,
-  };
+    this.state = {
+      width: 0,
+      cursorIndex: this.props.cursorIndex,
+    };
+  }
 
   componentDidMount() {
     this.updateWidth();
@@ -38,23 +46,67 @@ class Carousel extends React.PureComponent {
     this.setState({ width: this.container.current.clientWidth });
   };
 
+  prev = () => {
+    const { cursorIndex } = this.state;
+
+    if (this.props.enableLoop || cursorIndex > 0) {
+      this.setState({ cursorIndex: cursorIndex - 1 });
+    }
+  };
+
+  next = () => {
+    const { cursorIndex } = this.state;
+
+    if (this.props.enableLoop || cursorIndex < this.props.dataSource.length - 1) {
+      this.setState({ cursorIndex: cursorIndex + 1 });
+    }
+  };
+
   render() {
+    const { renderItem } = this.props;
+    const { width, cursorIndex } = this.state;
+
     return (
       <div className="carousel" ref={this.container}>
-        {this.props.dataSource.map((item, index) => (
-          <div
-            key={`${index + 1}`}
-            className="carousel-item"
-            style={{ width: this.state.width }}
-          >
-            {typeof item === 'string'
-              ? (
-                <Image width="100%" height="100%" src={item} />
-              )
-              : item.render(item)
-            }
-          </div>
-        ))}
+        <div
+          className="carousel-inner"
+          style={{
+            transform: `translateX(-${cursorIndex * width}px)`,
+          }}
+        >
+          {this.props.dataSource.map((item, index) => (
+            <div
+              key={`${index + 1}`}
+              className="carousel-item"
+              style={{
+                width,
+                left: index * width,
+              }}
+            >
+              {renderItem != null
+                ? renderItem(item)
+                : (
+                  <Image width="100%" height="100%" src={item} />
+                )}
+            </div>
+          ))}
+        </div>
+
+        {this.props.hasPageTurner && (
+          <React.Fragment>
+            <Clickable onClick={this.prev}>
+              <div className="carousel-control-prev">
+                <div className="carousel-control-prev-icon" />
+              </div>
+            </Clickable>
+
+            <Clickable onClick={this.next}>
+              <div className="carousel-control-next">
+                <div className="carousel-control-next-icon" />
+              </div>
+            </Clickable>
+          </React.Fragment>
+        )}
       </div>
     );
   }
